e632edd5ec52d369640ba77c9be185d3bcf5813b
Mohammed Yassen <mohammed.yassen6699@gmail.com>
2025-07-08 19:42:30 +0300

    Add Nullable For LastUpdatedOn Prop in DB

--- Bookify.Web/Controllers/CategoryController.cs
+++ Bookify.Web/Controllers/CategoryController.cs
@@ -15,6 +15,7 @@ namespace Bookify.Web.Controllers
 
         public IActionResult Index()
         {
+            //TODO: Add View Model For Category
             var Categories = _dbContext.Categories.Where(c=>c.IsDeleted == false).ToList(); // Get All Categories Where IsDeleted = False
 
             return View(Categories);
---
+++ Bookify.Web/Data/Migrations/20250708164121_make LastUpdatedOn for Category Nullable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Web.Data.Migrations
+{
+    public partial class makeLastUpdatedOnforCategoryNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdateOn",
+                table: "Categories",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastUpdateOn",
+                table: "Categories",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
