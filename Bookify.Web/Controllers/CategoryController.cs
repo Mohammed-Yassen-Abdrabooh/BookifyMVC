d2ef4f15d46b851b3ac6d9398b09abd999fdd0c7
Mohammed Yassen <mohammed.yassen6699@gmail.com>
2025-07-12 21:04:27 +0300

    - Add Create Action - Add Edit Action - Rename CategoryFormViewModel - Do Create and Edit Using The Same Form - Update IsDeleted Showing using Badges - Delete asp-action to Form => For using it   to Create or Edit

--- Bookify.Web/Controllers/CategoryController.cs
+++ Bookify.Web/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Bookify.Web.Data;
+﻿using Bookify.Web.Core.Models;
+using Bookify.Web.Core.ViewModels;
+using Bookify.Web.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookify.Web.Controllers
@@ -16,9 +18,71 @@ namespace Bookify.Web.Controllers
         public IActionResult Index()
         {
             //TODO: Add View Model For Category
-            var Categories = _dbContext.Categories.Where(c=>c.IsDeleted == false).ToList(); // Get All Categories Where IsDeleted = False
+            var Categories = _dbContext.Categories/*.Where(c=>c.IsDeleted == false)*/.ToList(); // Get All Categories Where IsDeleted = False "when un Comm Where ==> is not show it in View Index"
 
             return View(Categories);
         }
+
+        public IActionResult Create()
+        {
+            return View("Form");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryFormViewModel model)
+        {
+            if(!ModelState.IsValid)
+                return View("Form",model);
+
+            var category = new Category
+            {
+                Name= model.Name,
+            };
+            _dbContext.Categories.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var Categ = _dbContext.Categories.Find(id);
+            
+            if(Categ is null)
+                return NotFound();
+
+            var ViewModel = new CategoryFormViewModel
+            {
+                Id = Categ.Id,
+                Name = Categ.Name,
+            };
+
+            return View("Form", ViewModel);
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Form", model);
+
+            var Categ = _dbContext.Categories.Find(model.Id);
+
+            if (Categ is null)
+                return NotFound();
+
+            Categ.Name = model.Name;
+            Categ.LastUpdateOn = DateTime.Now;
+            _dbContext.Categories.Update(Categ);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+
+
+        }
+
+
     }
 }
---
+++ Bookify.Web/Core/ViewModels/CategoryFormViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookify.Web.Core.ViewModels
+{
+    public class CategoryFormViewModel
+    {
+        public int Id { get; set; }
+        [MaxLength(100,ErrorMessage = "Max Length Cannot Be More Than 100 Char") ]
+        public string Name { get; set; }=null!;
+    }
+}
