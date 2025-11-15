cbf11998bf6efcd8c1c17690f6f89a09cfd7de37
Mohammed Yassen <mohammed.yassen6699@gmail.com>
2025-11-15 23:41:24 +0200

    -Add More WhatsAppTemplate For Sessin27 and Closed

--- Bookify.Web/Core/Consts/WhatsAppTemplates.cs
+++ Bookify.Web/Core/Consts/WhatsAppTemplates.cs
@@ -1,7 +1,9 @@
 ﻿namespace Bookify.Web.Core.Consts
 {
     public static class WhatsAppTemplates
-    {
+    { 
+        // All is Utility Templates
+        public static string BookifySubscriberMessage = "bookify_subscriber_message";
         public static string BookifyMessage = "message_bookify";
         public static string MetaWelcomeMessage = "hello_world";
 
